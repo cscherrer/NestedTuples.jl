9a35e0ae66b5f5bcb6b59e0a332b381ef4594e6c
Chad Scherrer <chad.scherrer@gmail.com>
2020-12-08 15:37:08 -0800

    move stuff around

--- src/NestedTuples.jl
+++ src/NestedTuples.jl
@@ -2,26 +2,9 @@ module NestedTuples
 
 using Kaleido
 
-export Nested
-
-struct Nested{T}
-    value::T
-end
-
-import Base
-
-function Base.show(io::IO, x::Nested)
-    print(io, "Nested(", x.value, ")")
-end
-
 include("placeholder.jl")
 include("typelevel.jl")
 include("lenses.jl")
-
-import NamedTupleTools
-
-function Base.empty(x::Nested)
-    ℓ = lenses(x)
-end
+include("nested.jl")
 
 end
---
+++ src/nested.jl
@@ -0,0 +1,26 @@
+export Nested
+
+struct Nested{T}
+    value::T
+end
+
+import Base
+
+function Base.show(io::IO, x::Nested)
+    print(io, "Nested(", x.value, ")")
+end
+
+
+import NamedTupleTools
+
+function Base.empty(x::Nested)
+    ℓ = lenses(x)
+    n = length(ℓ)
+    set(x, batch(ℓ...), ntuple(i -> □, n))
+end
+
+Base.get(x::Nested, ℓ::Lens) = Nested(get(x.value, ℓ))
+
+Setfield.set(x::Nested, ℓ::Lens, v) = Nested(set(x.value, ℓ, v))
+
+Setfield.set(x::Nested, ℓ::Setfield.ComposedLens, v) = Nested(set(x.value, ℓ, v))
