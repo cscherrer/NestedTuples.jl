cd46b5df5cb24937850131d7d909194a6a81f215
Chad Scherrer <chad.scherrer@gmail.com>
2020-12-21 14:19:48 -0800

    exprify

---
+++ src/exprify.jl
@@ -0,0 +1,31 @@
+function exprify(x::NamedTuple; rename=true)
+    names = []
+    
+    function f(t::Tuple)
+        Expr(:tuple, t...)
+    end
+    
+    function f(t::NamedTuple)
+        kvs = zip(keys(t), values(t))
+        args = []
+        for (k,v) in kvs
+            push!(args, Expr(:(=), k, v))
+        end
+        Expr(:tuple, args...)
+    end
+    
+    function f(x)
+        if rename
+            name = gensym()
+        else
+            name = x
+        end
+        push!(names, name)
+        return name
+    end
+
+   
+    body = fold(f, x)
+
+    return (names, body)
+end
