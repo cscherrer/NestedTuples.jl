90e40ade7e270ae4d2691a44a5729e7827f70ed8
Chad Scherrer <chad.scherrer@gmail.com>
2020-12-28 08:58:35 -0800

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using NestedTuples
 using Test
 
 @testset "NestedTuples.jl" begin
-    # Write your tests here.
+    x = (a = (a = :a, b = :b), q = (l = :l, u = :u))
+
+    @test NestedTuples.exprify(x; rename=false) == ([:b, :h, :d, :j], :((b = (b = b, h = h), x = (d = d, j = j))))
+    
+    @test NestedTuples.flatten(x) == (:a, :b, :l, :u)
+
+    @test NestedTuples.keysort(x) == (a = (a = :a, b = :b), q = (l = :l, u = :u))
+    
+    @test NestedTuples.leaf_setter(x)(1,2,3,4) == (a = (a = 1, b = 2), q = (l = 3, u = 4))
+
+    @test NestedTuples.schema(x) == (a = (a = Symbol, b = Symbol), q = (l = Symbol, u = Symbol))
 end
