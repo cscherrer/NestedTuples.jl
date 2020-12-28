6d7feb35b70a08b4cfe2e4cc6c9cb43c09c40700
Chad Scherrer <chad.scherrer@gmail.com>
2020-12-28 08:37:41 -0800

    exclude nested.jl (for now, anyway)

--- src/NestedTuples.jl
+++ src/NestedTuples.jl
@@ -9,7 +9,7 @@ include("fold.jl")
 include("placeholder.jl")
 include("typelevel.jl")
 include("lenses.jl")
-include("nested.jl")
+# include("nested.jl")
 include("keysort.jl")
 
 include("exprify.jl")
