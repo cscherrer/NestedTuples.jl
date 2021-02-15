bdd102510ae81ed57061edc4842e22b6b357baec
Chad Scherrer <chad.scherrer@gmail.com>
2021-02-15 15:33:30 -0800

    summarize

---
+++ src/summarize.jl
@@ -0,0 +1,37 @@
+using StatsBase
+
+export summarize
+
+abstract type Summary end
+
+summarize(nt::NamedTuple) = mapnt(summarize, nt)
+
+summarize(ta::TupleArray) = summarize(getfield(ta, :data))
+
+
+###############################################################################
+# FullSummary
+
+struct FullSummary{T} <: Summary
+    data :: T
+end
+
+summarize(x) = FullSummary(x)
+
+Base.show(io, s::FullSummary) = print(io, s.data)
+
+###############################################################################
+# RealSummary
+
+struct RealSummary <: Summary
+    μ :: Float64
+    σ :: Float64
+end
+
+summarize(x::AbstractArray{<:Real}) = RealSummary(mean_and_std(x)...)
+
+
+function Base.show(io::IO, s::RealSummary)
+    io = IOContext(io, :compact => true)
+    print(io, s.μ, " ± ", s.σ)
+end
