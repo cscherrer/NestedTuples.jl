8f9fe9068212709501f273d4e2265b62e387acc5
Chad Scherrer <chad.scherrer@gmail.com>
2021-02-19 12:57:32 -0800

    clean up show method

--- src/summarize.jl
+++ src/summarize.jl
@@ -8,6 +8,8 @@ summarize(nt::NamedTuple) = rmap(summarize, nt)
 
 summarize(tv::TupleVector) = summarize(unwrap(tv))
 
+import Base
+
 
 ###############################################################################
 # FullSummary
@@ -22,31 +24,24 @@ Base.show(io, s::FullSummary) = print(io, s.data)
 
 ###############################################################################
 # RealSummary
-
+export RealSummary
 struct RealSummary <: Summary
     μ :: Float64
     σ :: Float64
 end
+
+Base.typeinfo_prefix(io::IO, ::AbstractArray{<:Summary}) = ("", false)
 
 function Base.show(io::IO, s::RealSummary)
     io = IOContext(io, :compact => true)
     σ = round(s.σ, sigdigits=2)
     μdigits = max(2, ceil(Int, log10(2) * (exponent(s.μ) - exponent(σ))) + 2)
     μ = round(s.μ, sigdigits = μdigits)
-    print(io, μ, " ± ", σ)
+    print(io, μ, "±", σ)
 end
 
 summarize(x::AbstractVector{<:Real}) = RealSummary(mean_and_std(x)...)
 
 function summarize(x::ArrayOfSimilarArrays)
-    
     [RealSummary(μ,σ) for (μ,σ) in mean_and_std.(x')]
-end
-
-export foo
-function foo(x)
-    s = RealSummary(mean_and_std(x)...)
-    σ = round(s.σ, sigdigits=2)
-    μ = round(s.μ, sigdigits=exponent(s.μ)-exponent(σ)+2, base=2)
-    return(μ,σ)
 end
