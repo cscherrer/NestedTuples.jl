16dd85bc4afb8ffdca3825665e4553334f7747e4
Chad Scherrer <chad.scherrer@gmail.com>
2021-04-30 07:57:30 -0700

    some more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using NestedTuples
 using Test
 
+using NestedTuples: with, TypelevelExpr
+
 @testset "NestedTuples.jl" begin
     x = (a = (a = :a, b = :b), q = (l = :l, u = :u))
 
@@ -19,4 +21,14 @@ using Test
     @test let nt = (x=1, y=(a=2, b=3))
         @with(nt, (x + @with(y, a + b))) == 6
     end
+
+    @test keysort(((b=1,a=2),(d=3,c=4))) == ((a = 2, b = 1), (c = 4, d = 3))
+
+    @test keysort(lazymerge((b=1,a=2),(d=3,c=4))) == lazymerge((a = 2, b = 1), (c = 4, d = 3))
+
+    @test keysort(NamedTuple()) == NamedTuple()
+
+    @test with(@__MODULE__, (x=1,), (y=2,), TypelevelExpr(:(x+y))) == 3
+
+    @test keysort(3) == 3
 end
